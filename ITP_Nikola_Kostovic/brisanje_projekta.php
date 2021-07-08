45610bf6090872557f5250274a6500571e339b5c
NikolaKostovic <86965228+NikolaKostovic@users.noreply.github.com>
2021-07-08 23:45:43 +0200

    Delete ITP_Nikola_Kostovic directory

--- ITP_Nikola_Kostovic/brisanje_projekta.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id_projekta = $_GET["id"];
-
-include "./db_connection.php";
-$connection->query("DELETE FROM `projekat` WHERE PROJEKAT_ID =  {$id_projekta};");
-$connection->query("DELETE FROM `zaduzenje` WHERE `PROJ_ID` =  {$id_projekta};");
-$connection->query("DELETE FROM `slika` WHERE `PROJEKAT_ID` =  {$id_projekta};");
-setcookie('notice', "Projekat je uspjesno obrisan!",  time()+10000);
-header("Location: index.php");
